3c4a39c871943370cca670fb7877b07a07db516e
Shrirang Patil <shrirangpatil1996@gmail.com>
2018-07-31 20:54:19 +0530

    Final Commit

---
+++ MyPrograms/FindS.py
@@ -0,0 +1,24 @@
+"""
+FindS without inductive bias
+"""
+noOfTrainEx = 4#int(input("Enter the no of training examples"))
+noOfAttri = 7
+trainEx = [["Sunny","Warm","Normal","Strong","Warm","Same","Yes"],
+           ["Sunny","Warm","High","Strong","Warm","Same","Yes"],
+           ["Sunny","Cold","High","Strong","Warm","Same","No"],
+           ["Sunny","Warm","High","Strong","Cool","Change","Yes"]]
+ex = []
+#print(trainEx)
+hypo = [0,0,0,0,0,0,0]
+for i in trainEx:
+    if i[6] == "Yes":
+        for j in range(len(i)):
+            if i[j] != hypo[j] and hypo[j] == 0:
+                hypo[j] = i[j]
+            elif i[j] == hypo[j]:
+                pass
+            else:
+                hypo[j] = "?"
+            print(hypo[0:6])
+print(hypo[0:6])
+
